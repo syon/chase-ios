c210e6dcf35a41f51e8752202761b141c7f50a74
syon <syon.xiii@gmail.com>
2017-03-05 17:02:30 +0900

    :art: Catalog

--- Chase/app/components/MainTab.js
+++ Chase/app/components/MainTab.js
@@ -1,49 +1,12 @@
 import React, { Component } from 'react'
 import { View, ListView, Image, StyleSheet, Text } from 'react-native'
 
+import Catalog from './Catalog'
+
 export default class extends Component {
-  constructor() {
-    super()
-    const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 })
-    this.state = {
-      dataSource: ds.cloneWithRows([]),
-    }
-  }
-
-  renderItem(item) {
-    return (
-      <View style={styles.item}>
-        <Image
-          style={styles.thumbnail}
-          source={{uri: 'https://facebook.github.io/react/img/logo_og.png'}}
-        />
-        <Text>{ item.title }</Text>
-      </View>
-    )
-  }
-
   render() {
     return (
-      <ListView
-        dataSource={this.state.dataSource.cloneWithRows(this.props.listData)}
-        renderRow={this.renderItem}
-        style={styles.itemList}
-      />
+      <Catalog {...this.props} />
     )
   }
 }
-
-const styles = StyleSheet.create({
-  itemList: {
-    flex: 1,
-    backgroundColor: '#FFCCAA',
-  },
-  item: {
-    flexDirection: 'row',
-    height: 50,
-  },
-  thumbnail: {
-    width: 50,
-    height: 50,
-  }
-});
