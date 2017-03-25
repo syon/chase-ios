a7c12f7ea97c4d39360cf1f54717735903d69416
syon <syon.xiii@gmail.com>
2017-03-25 22:28:30 +0900

    :+1: RefreshControl in TagsTab

--- Chase/app/screens/TagsTab.js
+++ Chase/app/screens/TagsTab.js
@@ -1,18 +1,59 @@
 import React, { Component } from 'react'
-import { View, Button, StyleSheet, Text } from 'react-native'
+import {
+  View,
+  Text,
+  Button,
+  StyleSheet,
+  ScrollView,
+  RefreshControl,
+} from 'react-native'
 
 class TagsTab extends Component {
+
+  state = {
+    isRefreshing: false,
+    loaded: 0,
+  }
+
+  _onRefresh = () => {
+    this.setState({isRefreshing: true});
+    setTimeout(() => {
+      this.setState({
+        loaded: this.state.loaded + 10,
+        isRefreshing: false,
+      });
+    }, 5000);
+  }
+
   render() {
     const { login, actions } = this.props;
     return (
-      <View style={styles.welcome}>
-        <Button onPress={actions.testPocketAdapter} title="API Test" />
-      </View>
+      <ScrollView
+        style={styles.scrollview}
+        refreshControl={
+          <RefreshControl
+            refreshing={this.state.isRefreshing}
+            onRefresh={this._onRefresh}
+            tintColor="#ff0000"
+            title="Loading..."
+            titleColor="#00ff00"
+            colors={['#ff0000', '#00ff00', '#0000ff']}
+            progressBackgroundColor="#ffff00"
+          />
+        }>
+        <View style={styles.welcome}>
+          <Button onPress={actions.testPocketAdapter} title="API Test" />
+          <Text>{ this.state.loaded }</Text>
+        </View>
+      </ScrollView>
     )
   }
 }
 
 const styles = StyleSheet.create({
+  scrollview: {
+    flex: 1,
+  },
   welcome: {
     margin: 10,
   },
