4a072c1121af52afdf242a62fc0755e05c796c49
syon <syon.xiii@gmail.com>
2017-04-22 20:16:20 +0900

    :sparkles: MyTron

--- Chase/app/utils/ReactotronConfig.js
+++ Chase/app/utils/ReactotronConfig.js
@@ -2,6 +2,41 @@ import Reactotron from 'reactotron-react-native'
 import { reactotronRedux } from 'reactotron-redux'
 import { HOST_ADDR } from 'react-native-dotenv'
 
+class MyTron {
+  static log(msg) {
+    Reactotron.log(msg)
+  }
+  
+  static display(obj) {
+    Reactotron.display(obj)
+  }
+
+  static start(label, obj) {
+    Reactotron.display({
+      name: label,
+      preview: 'START',
+      value: obj,
+    })
+  }
+
+  static info(label, obj) {
+    Reactotron.display({
+      name: label,
+      preview: 'INFO',
+      value: obj,
+    })
+  }
+
+  static error(label, obj) {
+    Reactotron.display({
+      name: 'ERROR',
+      preview: label,
+      value: obj,
+      important: true
+    })
+  }
+}
+
 if (__DEV__) {
   Reactotron
     .configure({
@@ -11,6 +46,6 @@ if (__DEV__) {
     .use(reactotronRedux())
     .connect()
 
-  console.tron = Reactotron
+  console.tron = MyTron
   Reactotron.clear()
 }
