350da756567322e6013613de4d4193a518c7c801
syon <syon.xiii@gmail.com>
2017-04-30 18:55:39 +0900

    :shower: clean

--- Chase/app/components/MyWebView.js
+++ Chase/app/components/MyWebView.js
@@ -1,41 +1,18 @@
 import React, { Component } from 'react'
-import {
-  View,
-  Button,
-  ProgressViewIOS,
-  StatusBar,
-} from 'react-native'
+import { View } from 'react-native'
 import WKWebView from 'react-native-wkwebview-reborn'
-
-// this.props.navigator.toggleDrawer({
-//   to: 'closed',
-//   side: 'left',
-//   animated: true
-// })
 
 export default class extends Component {
   constructor(props) {
     super(props);
-    this.state = { rate: 0 }
   }
 
   render() {
-    console.log('webview', this.props)
-    const wv = this.props.item ? (
-      <WKWebView
-        source={{ uri: this.props.item ? this.props.item.url : '' }}
-        onProgress={(progress) => this.setState({ rate: progress })}
-      />
-    ) : null
-    const pv = this.state.rate < 1 ? (<ProgressViewIOS progress={this.state.rate} />) : null
+    const { item } = this.props
+    const wv = item ? (<WKWebView source={{ uri: item ? item.url : '' }} />) : null
     return (
-      <View style={{flex: 1}}>
-        {/*<StatusBar hidden={true} />*/}
-        { pv }
+      <View style={{ flex: 1 }}>
         { wv }
-        <View>
-          {/*<Button onPress={() => {this.props.navigator.pop()}} title="←" />*/}
-        </View>
       </View>
     )
   }
