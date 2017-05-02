69737eb32291b0af7a49c34ba37db7157f3edfd8
syon <syon.xiii@gmail.com>
2017-05-02 17:45:05 +0900

    :+1: selectSceneBox in Config

--- Chase/app/screens/ConfigTab.js
+++ Chase/app/screens/ConfigTab.js
@@ -5,7 +5,12 @@ import { responsiveFontSize } from 'react-native-responsive-dimensions';
 
 class ConfigTab extends Component {
   constructor(props) {
-    super(props);
+    super(props)
+    this._onSelectSceneEdit = this._onSelectSceneEdit.bind(this)
+  }
+
+  _onSelectSceneEdit() {
+    return
   }
 
   render() {
@@ -24,6 +29,13 @@ class ConfigTab extends Component {
         <View>
           <Button onPress={actions.clearCatalogCache} style={styles.btn}>Clear Cache</Button>
         </View>
+        <View style={styles.selectSceneBox}>
+          <View style={styles.selectScene}>
+            <Button onPress={this._onSelectSceneEdit} style={styles.sceneBtn}>自宅</Button>
+            <Button onPress={this._onSelectSceneEdit} style={styles.sceneBtn}>職場</Button>
+            <Button onPress={this._onSelectSceneEdit} style={styles.sceneBtn}>暇つぶし</Button>
+          </View>
+        </View>
       </View>
     )
   }
@@ -33,17 +45,44 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
+    alignItems: 'stretch',
     marginBottom: 50,
+    backgroundColor: 'rgb(240, 239, 245)',
   },
   username: {
+    textAlign: 'center',
     fontSize: responsiveFontSize(2),
     margin: 20,
   },
   btn: {
     padding: 15,
     fontSize: responsiveFontSize(2.5),
-  }
+  },
+  selectSceneBox: {
+    justifyContent: 'center',
+    alignItems: 'stretch',
+    margin: 10,
+    marginTop: 30,
+    backgroundColor: '#fff',
+    borderRadius: 2,
+    shadowColor: '#000000',
+    shadowOffset: {
+      width: 0,
+      height: 1
+    },
+    shadowRadius: 1,
+    shadowOpacity: 0.2,
+  },
+  selectScene: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    paddingBottom: 5,
+  },
+  sceneBtn: {
+    fontSize: responsiveFontSize(2),
+    padding: 10,
+    overflow: 'hidden',
+  },
 })
 
 import { bindActionCreators } from 'redux'
