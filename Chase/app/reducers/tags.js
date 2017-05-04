f22ebaf0aa427978863f922fdfcee26442c424b4
syon <syon.xiii@gmail.com>
2017-05-05 02:10:06 +0900

    :bug: tags initial

--- Chase/app/reducers/tags.js
+++ Chase/app/reducers/tags.js
@@ -3,7 +3,7 @@ const initialState = {}
 export default function tags(state = initialState, action = {}) {
   switch (action.type) {
     case 'REFRESH_TAGS':
-      return action.tags
+      return action.tags || {}
     case 'LOGOUT_DONE':
       return initialState
     default:
