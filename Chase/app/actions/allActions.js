fafe2c4bec1b267635f57be8e1969364af8f61a6
syon <syon.xiii@gmail.com>
2017-05-07 00:10:44 +0900

    :shower: clean

--- Chase/app/actions/allActions.js
+++ Chase/app/actions/allActions.js
@@ -78,12 +78,8 @@ export function loginFromStorage() {
 function showLoginScreen(dispatch) {
   dispatch({ type: 'NEEDS_AUTH' })
   Navigation.showModal({
-    screen: "Chase.LoginScreen", // unique ID registered with Navigation.registerScreen
-    title: "Modal", // title of the screen as appears in the nav bar (optional)
-    passProps: {}, // simple serializable object that will pass as props to the modal (optional)
-    navigatorStyle: {}, // override the navigator style for the screen, see "Styling the navigator" below (optional)
-    navigatorButtons: {}, // override the nav buttons for the screen, see "Adding buttons to the navigator" below (optional)
-    animationType: 'slide-up' // 'none' / 'slide-up' , appear animation for the modal (optional, default 'slide-up')
+    screen: 'Chase.LoginScreen',
+    animationType: 'slide-up',
   })
 }
 
