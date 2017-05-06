e1c831074ea2d5c6b3ea6cbe560a81842972e69f
syon <syon.xiii@gmail.com>
2017-05-06 19:29:43 +0900

    :tada: Revived

--- Chase/app/app.ios.js
+++ Chase/app/app.ios.js
@@ -6,8 +6,8 @@ import { Navigation } from 'react-native-navigation'
 import './utils/ReactotronConfig'
 import { iconsMap, iconsLoaded } from './utils/AppIcons'
 
-// import * as screens from './screens'
-// screens.register()
+import * as screens from './screens'
+screens.register()
 
 global.storage = new Storage({
   storageBackend: AsyncStorage,
@@ -40,38 +40,38 @@ export default class App extends Component {
   startApp() {
     Navigation.startTabBasedApp({
       tabs: [
-        // {
-        //   label: 'Inbox',
-        //   screen: 'Chase.MainTabScreen',
-        //   icon: iconsMap['ios-filing-outline'],
-        //   selectedIcon: iconsMap['ios-filing'],
-        //   title: 'Inbox',
-        //   navigatorStyle,
-        // },
-        // {
-        //   label: 'Scene',
-        //   screen: 'Chase.SceneTabScreen',
-        //   icon: iconsMap['ios-glasses-outline'],
-        //   selectedIcon: iconsMap['ios-glasses'],
-        //   title: 'Scene',
-        //   navigatorStyle,
-        // },
-        // {
-        //   label: 'Tags',
-        //   screen: 'Chase.TagsTabScreen',
-        //   icon: iconsMap['ios-pricetags-outline'],
-        //   selectedIcon: iconsMap['ios-pricetags'],
-        //   title: 'Tags',
-        //   navigatorStyle,
-        // },
-        // {
-        //   label: 'Config',
-        //   screen: 'Chase.ConfigTabScreen',
-        //   icon: iconsMap['ios-options-outline'],
-        //   selectedIcon: iconsMap['ios-options'],
-        //   title: 'Config',
-        //   navigatorStyle,
-        // },
+        {
+          label: 'Inbox',
+          screen: 'Chase.MainTabScreen',
+          icon: iconsMap['ios-filing-outline'],
+          selectedIcon: iconsMap['ios-filing'],
+          title: 'Inbox',
+          navigatorStyle,
+        },
+        {
+          label: 'Scene',
+          screen: 'Chase.SceneTabScreen',
+          icon: iconsMap['ios-glasses-outline'],
+          selectedIcon: iconsMap['ios-glasses'],
+          title: 'Scene',
+          navigatorStyle,
+        },
+        {
+          label: 'Tags',
+          screen: 'Chase.TagsTabScreen',
+          icon: iconsMap['ios-pricetags-outline'],
+          selectedIcon: iconsMap['ios-pricetags'],
+          title: 'Tags',
+          navigatorStyle,
+        },
+        {
+          label: 'Config',
+          screen: 'Chase.ConfigTabScreen',
+          icon: iconsMap['ios-options-outline'],
+          selectedIcon: iconsMap['ios-options'],
+          title: 'Config',
+          navigatorStyle,
+        },
       ],
     })
   }
